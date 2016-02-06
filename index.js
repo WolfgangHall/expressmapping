816b8c7b71e562116be4af563f3bd5afda3a6d9c
Johall <johnalexander.w.hall@gmail.com>
2016-02-06 11:05:12 -0500

    initial commit with functioning server

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var app = express();
+var PORT = 8080;
+
+app.use('/js', express.static('public/js'));
+app.use('/css', express.static('public/css'));
+
+app.get("/", function(req, res){
+    res.sendFile(process.cwd() + "/views/home.html");
+});
+
+app.get("/register", function(req, res){
+    res.sendFile(process.cwd() + "/views/register.html");
+});
+
+app.get("/dashboard", function(req, res){
+    res.sendFile(process.cwd() + "/views/dashboard.html");
+});
+
+app.listen(PORT, function(){
+    console.log("Listening on Port %s", PORT);
+});
